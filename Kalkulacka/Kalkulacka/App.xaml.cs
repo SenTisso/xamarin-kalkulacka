8c263cf965ddc940ff085862d8e20068a422fedc
Jiří Hulmák <jirka.hulmak@gmail.com>
2020-10-10 18:11:59 +0200

    prida vypocet pro spravne rozmery tlacitek

--- Kalkulacka/Kalkulacka/App.xaml.cs
+++ Kalkulacka/Kalkulacka/App.xaml.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace Kalkulacka
 {
     public partial class App : Application
     {
+        /// <summary>promenna pro fixni sirku a vysku tlacitek</summary>
+        public static double buttonSize { get; set; }
+        /// <summary>hodnota, jake ma odsazeni tlacitek</summary>
+        public static double buttonSpacing { get; set; }
+
         public App()
         {
             InitializeComponent();
+
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            int columnCount = 4;
+            App.buttonSpacing = 15;
+
+            // (sirka displeje) - (velikost mezer * celkovy pocet vertikalnich mezer) / (pocet sloupcu)
+            App.buttonSize = ((mainDisplayInfo.Width / mainDisplayInfo.Density) - (App.buttonSpacing * (columnCount + 1))) / columnCount;
 
             MainPage = new MainPage();
         }
