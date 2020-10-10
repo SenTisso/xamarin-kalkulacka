f957a3ff8896e21b627a99054e0dd968acdb5ac8
Jiří Hulmák <jirka.hulmak@gmail.com>
2020-10-10 18:15:27 +0200

    prida event po kliknuti na tlacitka

--- Kalkulacka/Kalkulacka/MainPage.xaml.cs
+++ Kalkulacka/Kalkulacka/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Data;
+using System.Text.RegularExpressions;
 
 namespace Kalkulacka
 {
@@ -13,6 +16,15 @@ namespace Kalkulacka
         public MainPage()
         {
             InitializeComponent();
+        }
+
+
+        /// <summary>Event funkce, ktera je spustena po kliknuti na tlacitka.</summary>
+        /// <param name="sender">Objekt, na ktery se kliklo.</param>
+        /// <param name="args">Parametry eventu.</param>
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+
         }
     }
 }
